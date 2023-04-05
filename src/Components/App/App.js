d24758e11b2502fb240abe70d2602314355d18ef
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2023-04-05 17:12:09 +0200

    add SearchBar component with styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,6 @@
-// import logo from './logo.svg';
 import React from 'react';
 import './App.css';
+import {SearchBar} from '../SearchBar/SearchBar'
 
 class App extends React.Component {
 
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          {/* <SearchBar /> */}
+          <SearchBar />
           <div className="App-playlist">
             {/* {<SearchResults />} */}
             {/* {<Playlist />} */}
