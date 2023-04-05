fa9d54a1e42d4c754c544b3f69642fff1b7d14d3
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2023-04-05 20:32:58 +0200

    add state in App.js and pass it into SearchResults component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -4,6 +4,18 @@ import { SearchBar } from '../SearchBar/SearchBar'
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from  '../Playlist/Playlist';
 class App extends React.Component {
+  constructor(props) { 
+    super(props);
+    this.state = { 
+      searchResults: [{name: 'name1', artist: 'artist1', album: 'album1', id: 1}, 
+      {name: 'name2', artist: 'artist2', album: 'album2', id: 2},
+      {name: 'name3', artist: 'artist3', album: 'album3', id: 3},
+      {name: 'name4', artist: 'artist4', album: 'album4', id: 4},
+      {name: 'name5', artist: 'artist5', album: 'album5', id: 5}] 
+    };
+  }
+
+
 
   render() {
 
@@ -13,7 +25,7 @@ class App extends React.Component {
         <div className="App">
           <SearchBar />
           <div className="App-playlist">
-            {<SearchResults />}
+            {<SearchResults searchResults = {this.state.searchResults} />}
             {<Playlist />}
           </div>
         </div>
