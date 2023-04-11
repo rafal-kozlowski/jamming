ca3538d6f1a12032eba7ae295718e186017dd941
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2023-04-11 20:18:41 +0200

    create method that change the name of a Playlist

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -23,6 +23,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -50,7 +51,9 @@ class App extends React.Component {
     this.setState({playlistTracks: tracks});
   }
 
-
+  updatePlaylistName(name) {
+    this.setState({playlistName: name});
+  }
 
   render() {
 
@@ -66,7 +69,8 @@ class App extends React.Component {
             {<Playlist 
               playlistName = {this.state.playlistName} 
               playlistTracks = {this.state.playlistTracks}
-              onRemove = {this.removeTrack}/>}
+              onRemove = {this.removeTrack}
+              onNameChange={this.updatePlaylistName}/>}
           </div>
         </div>
       </div>
