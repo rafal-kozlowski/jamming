6c357b603e7768cbf149528f9f0b9bf5dd402968
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2023-04-12 12:44:01 +0200

    create search method for a SearchBar component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -25,6 +25,7 @@ class App extends React.Component {
     this.removeTrack = this.removeTrack.bind(this);
     this.updatePlaylistName = this.updatePlaylistName.bind(this);
     this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -61,25 +62,29 @@ class App extends React.Component {
     const trackURIs = tracks.map(track => track.uri);
   }
 
+  search(term) {
+    console.log(term);
+  }
+
   render() {
 
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} /> 
           <div className="App-playlist">
-            {<SearchResults 
+            <SearchResults 
               searchResults = {this.state.searchResults} 
               onAdd = {this.addTrack}
-            />}
-            {<Playlist 
+            />
+            <Playlist 
               playlistName = {this.state.playlistName} 
               playlistTracks = {this.state.playlistTracks}
               onRemove = {this.removeTrack}
               onNameChange={this.updatePlaylistName}
               onSave={this.savePlaylist}
-            />}
+            />
           </div>
         </div>
       </div>
