c2d7e2c630c9176181541ad7cb32a6e6eb74d43d
Rafal Kozlowski <raffal.kozlowski@gmail.com>
2023-04-14 09:55:55 +0200

    create search method connected to spotify api

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import SearchBar from '../SearchBar/SearchBar'
 import SearchResults from '../SearchResults/SearchResults';
 import Playlist from  '../Playlist/Playlist';
+
+import Spotify from '../../util/Spotify';
 class App extends React.Component {
   constructor(props) { 
     super(props);
@@ -63,7 +65,9 @@ class App extends React.Component {
   }
 
   search(term) {
-    console.log(term);
+    Spotify.search(term).then(searchResults => {
+      this.setState({searchResults: searchResults});
+    });
   }
 
   render() {
